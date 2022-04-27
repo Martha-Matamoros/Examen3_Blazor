1fc4b447e8ecb306403d95bdcf24a744b0967c1b
Martha Matamoros <luna27.mm@gmail.com>
2022-04-27 16:42:59 +0100

    commin2

--- Examen3/Blazor/Pages/Usuarios/Usuarios.razor.cs
+++ Examen3/Blazor/Pages/Usuarios/Usuarios.razor.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿using Blazor.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Modelos;
 namespace Blazor.Pages.Usuarios
 {
     partial class Usuarios
     {
+        [Inject] private IUsuarioServicio _usuarioServicio { get; set; }
 
+        private IEnumerable<Usuario> usuariosLista { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            usuariosLista = await _usuarioServicio.GetLista();
+        }
     }
 }
